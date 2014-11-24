27932bf1546907ef0a80f5044b2e81d7849023bf
Tsubasa-Kajinuma <tsubasa_kajinuma@voyagegroup.com>
2014-11-24 22:56:48 +0900

    GET /でリクエストがきたときに、index.htmlを返すようにした

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 var express = require('express'),
-    app = express()
+    app = express(),
+    ECT = require('ect'),
     port = process.env.PORT || 3000;
 
-app.get('/', function(req, res) {
-    console.log('hello');
-});
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ect');
+app.engine('ect', ECT({ watch: true, root: __dirname + '/views', ext: '.ect'}).render);
 
 var server = app.listen(port, function() {
-    console.log('listhend');
+    console.log('listhened the port');
 });
+
+
+var home = require('./controller/home');
+app.get('/', home.index);
