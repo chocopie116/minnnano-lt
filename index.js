9aef73108ba2ae2f8ef8b24e5cd07ea3314d8a48
Tsubasa-Kajinuma <Tsubasa_Kajinuma@voyagegroup.com>
2014-11-30 02:17:40 +0900

    [add] clickした分、socket.ioをとりあえず動くようにした

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 var express = require('express'),
     app = express(),
+    server = require('http').Server(app),
     path = require('path'),
     ECT = require('ect'),
-    port = process.env.PORT || 3000;
+    port = process.env.PORT || 3000,
+    io = require('socket.io')(server);
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ect');
 app.use(express.static(path.join(__dirname, 'public')));
 app.engine('ect', ECT({ watch: true, root: __dirname + '/views', ext: '.ect'}).render);
 
-var server = app.listen(port, function() {
-    console.log('listhened the port');
-});
+server.listen(port);
 
 
 var home = require('./controller/home');
 app.get('/', home.index);
+
+io.on('connection', function (socket) {
+  socket.on('uh-huh', function (data) {
+      console.log(data);
+    });
+});
