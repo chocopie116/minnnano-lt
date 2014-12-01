9f23a1434f6def934931b3ece0baf7b9b08bb3dc
Tsubasa-Kajinuma <tsubasa_kajinuma@voyagegroup.com>
2014-12-02 02:42:43 +0900

    [add] redis, mocha, power-assertを追加

---
+++ model/presentation.js
@@ -0,0 +1,5 @@
+var redis = require('redis'),
+    client = redis.createClient();
+
+exports.find = function() {
+}
---
+++ test/model/presentation.js
@@ -0,0 +1,7 @@
+var assert = require('power-assert');
+
+describe('test', function() {
+    it('sample_test', function() {
+        assert(0 === 0);
+    });
+});
