412277f9a0d87d20eb9d085151ae802dd878969d
Tsubasa-Kajinuma <tsubasa_kajinuma@voyagegroup.com>
2014-12-04 00:29:08 +0900

    Talkモデルを実装した

--- model/talk.js
+++ model/talk.js
@@ -1,17 +1,41 @@
-var count = 0,
-    speaker = '';
-
-
-function Talk(name) {
-};
-
-/**
- *
+/***
+ * インメモリで動作させる
  */
-exports.getCurrent = function() {
+var count = 0,
+    speaker = '名無しさん';
 
+var Talk = function() {
+    //ゲッター
+    this.getCount = function() {
+        return count;
+    }
+    this.getSpeaker = function() {
+        return speaker;
+    }
+
+    //テスト用リセットする
+    this.reset = function() {
+        count = 0;
+        speaker = '名無しさん';
+    }
+
+    //カウントアップ
+    this.countUp = function() {
+        count++;
+        return count;
+    }
+
+    this.nextSpeaker = function(name) {
+        if (speaker != name) {
+            speaker = name;
+            count = 0;
+        }
+    }
 }
 
-exports.create = function(name) {
+/**
+ * 現在のトークを取得する
+ */
+exports.findCurrent = function() {
     return new Talk();
 }
--- test/model/talk.js
+++ test/model/talk.js
@@ -1,7 +1,40 @@
-var assert = require('power-assert');
+var assert = require('power-assert')
+    Talk = require('../../model/talk');
 
-describe('問題は出題者をもっている', function() {
-    it('sample_test', function() {
-        assert(0 === 0);
+describe('Talkモデルのテスト', function() {
+    it('何もしていない状態はcountは0で、名前は名無しさんが返るはず', function() {
+        var talk = Talk.findCurrent();
+
+        assert(0 === talk.getCount());
+        assert('名無しさん' === talk.getSpeaker());
+    });
+
+    it ('カウントアップしたら、数字が1上がる', function() {
+        var talk = Talk.findCurrent();
+        var afterCount = talk.countUp();
+
+        assert(1 === afterCount);
+    });
+
+    it ('１つのプロセスで実行すればcountは１のまま', function() {
+        var talk = Talk.findCurrent();
+
+        assert(1 === talk.getCount());
+    });
+
+    it('次のスピーカーになったらcountは0', function() {
+        var talk = Talk.findCurrent();
+        talk.nextSpeaker('佐藤さん');
+
+        assert(0 === talk.getCount());
+        assert('佐藤さん' === talk.getSpeaker());
+    });
+
+    it('リセットしたら、countは0で、名前も名無しになる', function() {
+        var talk = Talk.findCurrent();
+        talk.reset();
+
+        assert(0 === talk.getCount());
+        assert('名無しさん' === talk.getSpeaker());
     });
 });
